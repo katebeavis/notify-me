dd5793d6683a0e00346a62f331bf2a1e27861b55
Kate Beavis <katebeavis84@gmail.com>
2020-05-14 17:52:00 +0100

    Refactor

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -7,47 +7,41 @@ import { INotificationCallbackProps, NotificationType } from '../../Types';
 const Home = () => {
   const { notify } = useNotify();
 
-  const neededNotifications: INotificationCallbackProps[] = [
-    {
-      type: NotificationType.NEEDED,
-      title: 'Needed notification',
-      text: 'Needed',
-      onMore: () => alert('woooooo'),
-    },
-    {
-      type: NotificationType.NEEDED,
-      title: 'Needed notification',
-      text: 'Needed',
-      onMore: () => alert('woooooo'),
-    },
-  ];
+  const needed = {
+    type: NotificationType.NEEDED,
+    title: 'Needed notification',
+    text: 'Needed',
+    onMore: () => alert('woooooo'),
+  };
 
-  const successNotifications: INotificationCallbackProps[] = [
-    {
-      type: NotificationType.SUCCESS,
-      title: 'Success notification',
-      text: 'Success',
-      onMore: () => alert('woooooo'),
-    },
-  ];
+  const success = {
+    type: NotificationType.SUCCESS,
+    title: 'Success notification',
+    text: 'Success',
+    onMore: () => alert('woooooo'),
+  };
 
-  const errorNotifications: INotificationCallbackProps[] = [
-    {
-      type: NotificationType.ERROR,
-      title: 'Error notification',
-      text: 'Error',
-      onMore: () => alert('woooooo'),
-    },
-  ];
+  const error = {
+    type: NotificationType.ERROR,
+    title: 'Error notification',
+    text: 'Error',
+    onMore: () => alert('woooooo'),
+  };
 
-  const promoNotifications: INotificationCallbackProps[] = [
-    {
-      type: NotificationType.PROMOTIONAL,
-      title: 'Promo notification',
-      text: 'Promo',
-      onMore: () => alert('woooooo'),
-    },
-  ];
+  const promo = {
+    type: NotificationType.PROMOTIONAL,
+    title: 'Promo notification',
+    text: 'Promo',
+    onMore: () => alert('woooooo'),
+  };
+
+  const neededNotifications: INotificationCallbackProps[] = [needed, needed];
+
+  const successNotifications: INotificationCallbackProps[] = [success];
+
+  const errorNotifications: INotificationCallbackProps[] = [error];
+
+  const promoNotifications: INotificationCallbackProps[] = [promo];
 
   useEffect(() => {
     const notifications = [
@@ -70,18 +64,12 @@ const Home = () => {
   return (
     <>
       <Header>Notify me</Header>
-      <button
-        onClick={() =>
-          notify({
-            type: 'success',
-            title: 'A title',
-            text: 'Text',
-            onMore: () => alert('woooooo'),
-          })
-        }
-      >
-        Trigger
-      </button>
+      <ButtonContainer>
+        <Button onClick={() => notify(needed)}>Notify me of needed</Button>
+        <Button onClick={() => notify(success)}>Notify me of success</Button>
+        <Button onClick={() => notify(error)}>Notify me of error</Button>
+        <Button onClick={() => notify(promo)}>Notify me of promo</Button>
+      </ButtonContainer>
     </>
   );
 };
@@ -90,4 +78,24 @@ export default Home;
 
 const Header = styled.h1`
   text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 15%;
+`;
+
+const Button = styled.button`
+  padding: 1em;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 4px;
+  margin-bottom: 2em;
+  box-shadow: 3px 3px 15px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(0, 0, 0, 0.25);
+  }
 `;
