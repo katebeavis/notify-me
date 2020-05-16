eefe3e376ad98558aa3f6b75212de2797b68d669
Kate Beavis <katebeavis84@gmail.com>
2020-05-16 09:27:50 +0100

    change build path

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   output: {
     filename: 'build.js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../build'),
     publicPath: '/',
   },
   resolve: {
